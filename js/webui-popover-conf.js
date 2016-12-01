7402b71410eaad0a4ff82f0935a0cea5e06d4be7
Deliaz <D3liaz@gmail.com>
2016-12-02 02:18:25 +0700

    Styles for event settings form

--- js/webui-popover-conf.js
+++ js/webui-popover-conf.js
@@ -4,27 +4,31 @@ export default function getConfig($container) {
     return {
         container: $container || document.body,
         content: `
-    
-    <form class="js-event-edit-form">
-    <input type="text" class="js-event-title-input" name="title" placeholder="Заголовок">
-    
-    <button class="js-event-save-button" type="submit">Сохранить</button>
-    <button class="js-event-delete-button" type="button">Удалить</button>
-    </form>
-
+        
+        <form class="event-edit-form js-event-edit-form">            
+            <label for="event-title">Заголовок события</label>
+            <input type="text" class="event-title-input js-event-title-input"
+                id="event-title"
+                name="title" 
+                placeholder="Заголовок">
+            
+            <button class="event-save-button js-event-save-button" type="submit">Сохранить</button>
+            <button class="event-delete-button js-event-delete-button" type="button">Удалить</button>
+        </form>
 `,
         trigger: 'manual',
         animation: 'fade',
         closeable: true,
         multi: false,
         dismissible: true,
+        width: 300,
 
         onShow: $element => {
 
         },
 
         onHide: $element => {
-          $element.find(SELECTORS.eventTitleInput).val('');
+            $element.find(SELECTORS.eventTitleInput).val('');
         }
     }
 
